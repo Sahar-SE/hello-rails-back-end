fe43358b8c5937bd03b251cceceec519e2e1dd65
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-11-02 17:25:50 +0430

    Update the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  namespace :api do
-    namespace :v1 do
-      resources :greetings
-    end
-  end
+ get 'api/v1/greeting', to: 'application#greeting'
+ root 'application#greeting'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
